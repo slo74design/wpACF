4c37b9a8ab8f5fbe7edd023bf50a5bc3c71048e5
Slo74design <salvologiudice@gmail.com>
2023-10-15 18:28:43 +0100

    templates
    - template_parts folder (init)
    - @prettier/plugin-php

--- wp-content/themes/wpAcfTheme/index.php
+++ wp-content/themes/wpAcfTheme/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Main Template file
  * @package rsq
@@ -8,20 +9,26 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
+	<?php wp_body_open(); ?>
 	<div id="content" class="site-content">
 		<div id="primary" class="content-area">
-			<main id="main" class="site-main bg-slate-500 min-h-screen">
-        <h1 class="text-slate-50 text-lg font-thin">RSQ Salva oper</h1>
-      </main>
-    </div>
-  </div>
+			<main id="main" class="site-main bg-slate-500">
+				<header>Header</header>
+				<footer>Foote</footer>
+				<div>
+					<h1>sasas</h1>
+				</div>
+			</main>
+		</div>
+	</div>
 </body>
+
 </html>
